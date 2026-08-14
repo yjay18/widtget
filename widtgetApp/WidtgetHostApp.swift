e2ea05cb836fa3d5c63deb6e5b2166fb374b7342
Yuuv Jauhari <jauhariyuuv@gmail.com>
2026-08-14 17:02:49 +0100

    Reuse settings window on widget clicks

--- widtgetApp/WidtgetHostApp.swift
+++ widtgetApp/WidtgetHostApp.swift
@@ -3,12 +3,13 @@ import SwiftUI
 @main
 struct WidtgetHostApp: App {
     var body: some Scene {
-        WindowGroup {
+        Window("widtget", id: "settings") {
             WidgetSettingsView()
                 .frame(width: 420)
         }
         .defaultSize(width: 420, height: 620)
         .windowResizability(.contentSize)
+        .handlesExternalEvents(matching: ["refresh"])
         .commands {
             CommandGroup(replacing: .newItem) { }
         }
